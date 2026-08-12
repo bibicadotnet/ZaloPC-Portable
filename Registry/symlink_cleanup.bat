a9fc6786d977c530415d1ef194d18f31d2a6a7d3
bibica <119997727+bibicadotnet@users.noreply.github.com>
2026-08-12 09:04:20 +0700

    Add files via upload

--- Registry/symlink_cleanup.bat
+++ Registry/symlink_cleanup.bat
@@ -1,5 +1,8 @@
 @echo off
-:: Clean up junctions
 if exist "%APPDATA%\ZaloData" rmdir "%APPDATA%\ZaloData"
 if exist "%LOCALAPPDATA%\ZaloPC" rmdir "%LOCALAPPDATA%\ZaloPC"
 if exist "%USERPROFILE%\Documents\Zalo Received Files" rmdir "%USERPROFILE%\Documents\Zalo Received Files"
+
+if exist "%APPDATA%\ZaloData_bak" rename "%APPDATA%\ZaloData_bak" "ZaloData"
+if exist "%LOCALAPPDATA%\ZaloPC_bak" rename "%LOCALAPPDATA%\ZaloPC_bak" "ZaloPC"
+if exist "%USERPROFILE%\Documents\Zalo Received Files_bak" rename "%USERPROFILE%\Documents\Zalo Received Files_bak" "Zalo Received Files"
--- Registry/symlink_setup.bat
+++ Registry/symlink_setup.bat
@@ -1,12 +1,39 @@
 @echo off
 set "PORTABLE_ROOT=%~dp0.."
 
-:: Create target directories if they don't exist in portable folder
 if not exist "%PORTABLE_ROOT%\Data\Roaming\ZaloData" mkdir "%PORTABLE_ROOT%\Data\Roaming\ZaloData"
 if not exist "%PORTABLE_ROOT%\Data\Local\ZaloPC" mkdir "%PORTABLE_ROOT%\Data\Local\ZaloPC"
 if not exist "%PORTABLE_ROOT%\Data\Documents\Zalo Received Files" mkdir "%PORTABLE_ROOT%\Data\Documents\Zalo Received Files"
 
-:: Create junctions
-if not exist "%APPDATA%\ZaloData" mklink /j "%APPDATA%\ZaloData" "%PORTABLE_ROOT%\Data\Roaming\ZaloData"
-if not exist "%LOCALAPPDATA%\ZaloPC" mklink /j "%LOCALAPPDATA%\ZaloPC" "%PORTABLE_ROOT%\Data\Local\ZaloPC"
-if not exist "%USERPROFILE%\Documents\Zalo Received Files" mklink /j "%USERPROFILE%\Documents\Zalo Received Files" "%PORTABLE_ROOT%\Data\Documents\Zalo Received Files"
+dir /al "%APPDATA%" 2>nul | findstr /c:"ZaloData" >nul
+if %errorlevel% equ 0 (
+    rmdir "%APPDATA%\ZaloData"
+) else (
+    if exist "%APPDATA%\ZaloData" (
+        if exist "%APPDATA%\ZaloData_bak" rmdir /s /q "%APPDATA%\ZaloData_bak"
+        rename "%APPDATA%\ZaloData" "ZaloData_bak"
+    )
+)
+mklink /j "%APPDATA%\ZaloData" "%PORTABLE_ROOT%\Data\Roaming\ZaloData"
+
+dir /al "%LOCALAPPDATA%" 2>nul | findstr /c:"ZaloPC" >nul
+if %errorlevel% equ 0 (
+    rmdir "%LOCALAPPDATA%\ZaloPC"
+) else (
+    if exist "%LOCALAPPDATA%\ZaloPC" (
+        if exist "%LOCALAPPDATA%\ZaloPC_bak" rmdir /s /q "%LOCALAPPDATA%\ZaloPC_bak"
+        rename "%LOCALAPPDATA%\ZaloPC" "ZaloPC_bak"
+    )
+)
+mklink /j "%LOCALAPPDATA%\ZaloPC" "%PORTABLE_ROOT%\Data\Local\ZaloPC"
+
+dir /al "%USERPROFILE%\Documents" 2>nul | findstr /c:"Zalo Received Files" >nul
+if %errorlevel% equ 0 (
+    rmdir "%USERPROFILE%\Documents\Zalo Received Files"
+) else (
+    if exist "%USERPROFILE%\Documents\Zalo Received Files" (
+        if exist "%USERPROFILE%\Documents\Zalo Received Files_bak" rmdir /s /q "%USERPROFILE%\Documents\Zalo Received Files_bak"
+        rename "%USERPROFILE%\Documents\Zalo Received Files" "Zalo Received Files_bak"
+    )
+)
+mklink /j "%USERPROFILE%\Documents\Zalo Received Files" "%PORTABLE_ROOT%\Data\Documents\Zalo Received Files"
